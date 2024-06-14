cb28e819ee7d037e61dd2166b3d9d62cacb825a2
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-06-13 20:04:50 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/app_ventas_idat/AppVentasIdatApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_ventas_idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVentasIdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVentasIdatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_ventas_idat/AppVentasIdatApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_ventas_idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVentasIdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
