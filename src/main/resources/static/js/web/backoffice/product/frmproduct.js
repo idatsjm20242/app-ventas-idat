c5026bd5f4b5cfa6d5abf3799539c28ce3b712f6
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-06-27 22:35:40 -0500

    eventos click terminados

--- src/main/resources/static/js/web/backoffice/product/frmproduct.js
+++ src/main/resources/static/js/web/backoffice/product/frmproduct.js
@@ -9,5 +9,16 @@ $(document).on("click", "#btnagregar", function(){
     $("#modalproduct").modal("show");
 })
 $(document).on("click", ".btnactualizar", function(){
+    $("#txtnomproduct").val($(this).attr("data-prodname"));
+    $("#txtunitprice").val($(this).attr("data-produnitprice"));
+    $("#switchproduct").show();
+    if ($(this).attr("data-proddiscontinued") === "true"){
+        $("#cbdiscontinued").prop("checked", true);
+    }else{
+        $("#cbdiscontinued").prop("checked", false);
+    }
+    $("#hddcodprod").val($(this).attr("data-prodcod"));
+    $("#cbocategory").empty();
+    $("#cbosupplier").empty();
     $("#modalproduct").modal("show");
 })
