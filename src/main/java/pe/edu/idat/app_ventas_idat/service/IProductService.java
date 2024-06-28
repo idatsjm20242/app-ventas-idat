882c2779e1298e7ec359a50de193392bfd8ef165
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-06-27 20:53:06 -0500

    productDTO en service

--- src/main/java/pe/edu/idat/app_ventas_idat/service/IProductService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/IProductService.java
@@ -1,12 +1,13 @@
 package pe.edu.idat.app_ventas_idat.service;
 
 import pe.edu.idat.app_ventas_idat.model.bd.Product;
+import pe.edu.idat.app_ventas_idat.model.dto.ProductDto;
 
 import java.util.List;
 
 public interface IProductService {
 
-    List<Product> listarProductos();
-    Product guardarProducto(Product product);
+    List<ProductDto> listarProductos();
+    Product guardarProducto(ProductDto product);
     Product obtenerProducto(Integer idproducto);
 }
--- src/main/java/pe/edu/idat/app_ventas_idat/service/ProductService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/ProductService.java
@@ -3,8 +3,10 @@ package pe.edu.idat.app_ventas_idat.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pe.edu.idat.app_ventas_idat.model.bd.Product;
+import pe.edu.idat.app_ventas_idat.model.dto.ProductDto;
 import pe.edu.idat.app_ventas_idat.repository.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
 @Service
@@ -12,12 +14,27 @@ public class ProductService implements IProductService {
     private ProductRepository productRepository;
 
     @Override
-    public List<Product> listarProductos() {
-        return productRepository.findAll();
+    public List<ProductDto> listarProductos() {
+        List<ProductDto> productDtoList = new ArrayList<>();
+        for (Product prod :
+                productRepository.findAll()) {
+            ProductDto productDto = ProductDto.builder()
+                    .productid(prod.getProductid())
+                    .productname(prod.getProductname())
+                    .discontinued(prod.isDiscontinued())
+                    .unitprice(prod.getUnitprice())
+                    .categoryid(prod.getCategory().getCategoryid())
+                    .categoryname(prod.getCategory().getCategoryname())
+                    .supplierid(prod.getSupplier().getSupplierid())
+                    .companyname(prod.getSupplier().getCompanyname())
+                    .build();
+            productDtoList.add(productDto);
+        }
+        return productDtoList;
     }
     @Override
-    public Product guardarProducto(Product product) {
-        return productRepository.save(product);
+    public Product guardarProducto(ProductDto product) {
+        return productRepository.save(new Product());
     }
     @Override
     public Product obtenerProducto(Integer idproducto) {
