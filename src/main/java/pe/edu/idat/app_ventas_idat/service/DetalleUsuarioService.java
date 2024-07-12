29fa9090d604c0b4084e8237528ca810906dc96c
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-07-11 22:18:39 -0500

    Detalle usuario service terminado

--- src/main/java/pe/edu/idat/app_ventas_idat/service/DetalleUsuarioService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/DetalleUsuarioService.java
@@ -1,4 +1,54 @@
 package pe.edu.idat.app_ventas_idat.service;
 
-public class DetalleUsuarioService {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.edu.idat.app_ventas_idat.model.bd.Rol;
+import pe.edu.idat.app_ventas_idat.model.bd.Usuario;
+import pe.edu.idat.app_ventas_idat.model.dto.UsuarioSecurityDto;
+import pe.edu.idat.app_ventas_idat.repository.UsuarioRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class DetalleUsuarioService
+        implements UserDetailsService {
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository
+                .findByNomusuario(username);
+        return obtenerUsuarioSecurity(usuario,
+                rolesUsuario(usuario.getRoles()));
+    }
+    private List<GrantedAuthority> rolesUsuario(
+            Set<Rol> roles
+    ){
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for(Rol rol: roles){
+            authorityList.add(new
+                    SimpleGrantedAuthority(rol.getNomrol()));
+        }
+        return authorityList;
+    }
+    private UserDetails obtenerUsuarioSecurity(
+            Usuario usuario, List<GrantedAuthority> authorityList
+    ){
+        UsuarioSecurityDto securityDto = new
+                UsuarioSecurityDto(usuario.getNomusuario(),
+                usuario.getPassword(),
+                usuario.getActivo(),
+                true,
+                true,
+                true, authorityList);
+        securityDto.setEmail(usuario.getEmail());
+        securityDto.setIdusuario(usuario.getIdusuario());
+        return securityDto;
+    }
 }
