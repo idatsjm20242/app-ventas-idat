a7e8645d222b839f34cc4f5c73a30aa4003ab48d
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-07-11 21:55:55 -0500

    Usuario service v1.0

--- src/main/java/pe/edu/idat/app_ventas_idat/service/IUsuarioService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/IUsuarioService.java
@@ -1,4 +1,15 @@
 package pe.edu.idat.app_ventas_idat.service;
 
+import pe.edu.idat.app_ventas_idat.model.bd.Usuario;
+
+import java.util.List;
+
 public interface IUsuarioService {
+
+    Usuario obtenerUsuarioxNomusuario(String nomusuario);
+    Usuario guardarUsuario(Usuario usuario);
+    List<Usuario> listarUsuario();
+    Usuario obtenerUsuarioxId(Integer id);
+    void actualizarUsuario(Usuario usuario);
+
 }
--- src/main/java/pe/edu/idat/app_ventas_idat/service/UsuarioService.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/service/UsuarioService.java
@@ -1,4 +1,52 @@
 package pe.edu.idat.app_ventas_idat.service;
 
-public class UsuarioService {
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pe.edu.idat.app_ventas_idat.model.bd.Rol;
+import pe.edu.idat.app_ventas_idat.model.bd.Usuario;
+import pe.edu.idat.app_ventas_idat.repository.RolRepository;
+import pe.edu.idat.app_ventas_idat.repository.UsuarioRepository;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+@AllArgsConstructor
+@Service
+public class UsuarioService implements IUsuarioService {
+    private UsuarioRepository usuarioRepository;
+    private RolRepository rolRepository;
+    BCryptPasswordEncoder bCryptPasswordEncoder
+            = new BCryptPasswordEncoder();
+    @Override
+    public Usuario obtenerUsuarioxNomusuario(String nomusuario) {
+        return usuarioRepository.findByNomusuario(nomusuario);
+    }
+
+    @Override
+    public Usuario guardarUsuario(Usuario usuario) {
+        usuario.setPassword(bCryptPasswordEncoder
+                .encode("123456"));
+        usuario.setActivo(true);
+        Rol usuarioRol = rolRepository.findByNomrol("USER");
+        usuario.setRoles(new
+                HashSet<>(Arrays.asList(usuarioRol)));
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public List<Usuario> listarUsuario() {
+        return null;
+    }
+
+    @Override
+    public Usuario obtenerUsuarioxId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void actualizarUsuario(Usuario usuario) {
+
+    }
 }
