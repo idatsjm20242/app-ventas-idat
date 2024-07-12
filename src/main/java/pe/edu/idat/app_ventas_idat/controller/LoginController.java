8e62558b3b53f9943afe4b878757218d0a8ce1b6
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-07-11 22:46:10 -0500

    Login controller terminado

--- src/main/java/pe/edu/idat/app_ventas_idat/controller/LoginController.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/controller/LoginController.java
@@ -1,4 +1,37 @@
 package pe.edu.idat.app_ventas_idat.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.idat.app_ventas_idat.model.dto.UsuarioSecurityDto;
+
+@Controller
+@RequestMapping("/auth")
 public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+    }
+    @GetMapping("/login-success")
+    public String loginSuccess(){
+        return "redirect:/auth/dashboard";
+    }
+
+    @GetMapping("/dashboard")
+    public String dasboard(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        UserDetails userDetails = (UserDetails)
+                SecurityContextHolder.getContext()
+                        .getAuthentication().getPrincipal();
+        UsuarioSecurityDto securityDto =
+                (UsuarioSecurityDto) userDetails;
+        session.setAttribute("nomusuario",
+                securityDto.getEmail());
+        return "auth/home";
+    }
+
 }
